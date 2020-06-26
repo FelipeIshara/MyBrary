a0aedc867740dfbb70dc0cc2eb3445cc69f3ba09
SEUMERDA <felipeishara@hotmail.com>
2020-06-26 15:32:57 -0300

    set MVC

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const espressLayouts = require('express-ejs-layouts')
+const indexRouter = require('./routes/index')
 
+//Configuração
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-
+app.use(espressLayouts)
 app.use(express.static('public'))
+
+app.use('/', indexRouter)
+
+
 
 app.listen(process.env.PORT || 3000)
