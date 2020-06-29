3a7227f4306162f4dd2c29dd800090372a46e011
SEUMERDA <felipeishara@hotmail.com>
2020-06-28 23:35:36 -0300

    errorMessage, displaiyn authors, creating

--- routes/author.js
+++ routes/author.js
@@ -2,19 +2,35 @@ const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
 
-router.get('/', (req,res) =>{
-    res.render('../views/authors/index.ejs')
+router.get('/', async (req,res) =>{
+    try{
+        const authors = await Author.find({})
+        res.render('../views/authors/index.ejs', {authors: authors})
+    } catch {
+        res.redirect('/')
+    }    
 })
 
 router.get('/new', (req,res)=>{
     res.render('authors/new.ejs', {author: new Author()})
 })
 
-router.post('/', (req,res)=>{
+router.post('/', async (req,res)=>{
     const author = new Author({
         name: req.body.name 
     })
-    author.save((err, newAuthor) => {
+    try {
+        const newAuthor = await author.save()
+        // res.redirect('authors/${newAuthor.id}')
+        res.redirect('authors')
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
+    
+    /*author.save((err, newAuthor) => {
         if (err) {
             res.render('authors/new', {
                 author: author,
@@ -24,7 +40,7 @@ router.post('/', (req,res)=>{
             // res.redirect('authors/${newAuthor.id}')
             res.redirect('authors')
         }
-    })
+    })*/
 })
 
 module.exports = router;
